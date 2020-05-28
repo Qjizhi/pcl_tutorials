c075e92d6fa2910df044daca95951513434a1b05
Qjizhi <feng.qiao@rwth-aachen.de>
2020-05-28 14:50:12 +0800

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# ground_filter_pcl_RANSAC
-filter ground using pcl lib
+## enviroment
+```
+ubuntu 16.04
+pcl 1.7
+```
+## usage
+```
+mkdir build
+cd build
+cmake ..
+make
+cd ..
+./pcl_test
+```
+You can use `pcl_viewer` tool to visualize the result: `pcl_viewer pointcloud_files/000000_filtered.pcd`.    
+
+Comparison before & after filtering ground(point cloud file is from kitti dataset):  
+![](images/ground_filter.png)
---
+++ pcl_test.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <pcl/io/pcd_io.h>
+#include <pcl/point_types.h>
+#include <pcl/filters/extract_indices.h>
+#include <pcl/ModelCoefficients.h>
+#include <pcl/segmentation/sac_segmentation.h>
+
+using namespace std;
+
+void detectObjectsOnCloud(pcl::PointCloud<pcl::PointXYZ>::Ptr &cloud, pcl::PointCloud<pcl::PointXYZ>::Ptr &cloud_filtered)
+{
+    if (cloud->size() > 0)
+    {
+        pcl::ModelCoefficients::Ptr coefficients(new pcl::ModelCoefficients);
+        pcl::PointIndices::Ptr inliers(new pcl::PointIndices);
+        pcl::SACSegmentation<pcl::PointXYZ> seg;
+        seg.setOptimizeCoefficients(true);
+        seg.setModelType(pcl::SACMODEL_PLANE);
+        seg.setMethodType(pcl::SAC_RANSAC);
+        // you can modify the parameter below
+  			seg.setMaxIterations(10000);
+        seg.setDistanceThreshold(0.15);
+        seg.setInputCloud(cloud);
+        seg.segment(*inliers, *coefficients);
+        if (inliers->indices.size() == 0)
+        {
+            cout<<"error! Could not found any inliers!"<<endl;
+        }
+        // extract ground
+        pcl::ExtractIndices<pcl::PointXYZ> extractor;
+        extractor.setInputCloud(cloud);
+        extractor.setIndices(inliers);
+        extractor.setNegative(true);
+        extractor.filter(*cloud_filtered);
+        // vise-versa, remove the ground not just extract the ground
+        // just setNegative to be true
+        cout << "filter done."<<endl;
+    }
+    else
+    {
+        cout<<"no data!"<<endl;
+    }
+}
+
+int main (int argc, char** argv)
+{
+    pcl::PointCloud<pcl::PointXYZ>::Ptr cloud (new pcl::PointCloud<pcl::PointXYZ>);
+    pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_filtered (new pcl::PointCloud<pcl::PointXYZ>);
+
+    // Fill in the cloud data
+    pcl::PCDReader reader;
+    // Replace the path below with the path where you saved your file
+    reader.read<pcl::PointXYZ> ("pointcloud_files/000000.pcd", *cloud);
+    detectObjectsOnCloud(cloud, cloud_filtered);
+    pcl::PCDWriter writer;
+    writer.write<pcl::PointXYZ> ("pointcloud_files/000000_filtered.pcd", *cloud_filtered, false);
+
+    return (0);
+}
